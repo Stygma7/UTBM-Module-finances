785b130fbf89945e4eb5fa30fa45a0f4f61360f6
Izro-hugo <hugo90jak@gmail.com>
2021-04-27 11:19:30 +0200

    Toto est arrive

--- Node/index.js
+++ Node/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+var path = require('path');
 
 app.get('/finance', (req,res) => {
     res.send("<h4>Finance<h4>")
 })
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../develop_Form/toto.html'));
+});
 
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
