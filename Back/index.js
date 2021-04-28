181c2465fda6530229ce593292bbd4e82fbb7623
Styg <matthieu.hirth.68@gmail.com>
2021-04-28 17:50:51 +0200

    test de materialize

--- Back/index.js
+++ Back/index.js
@@ -8,6 +8,9 @@ const dirPath = path.join(__dirname, '/../Front');
 app.get('/finance', (req,res) => {
     res.sendFile(path.join(dirPath + '/finance.html'));
 })
+app.get('/', (req,res) => {
+    res.sendFile(path.join(dirPath + '/finance.html'));
+})
 app.listen(8080, () => {
-    console.log("Serveur à l'écoute")
+    console.log("Serveur à l'écoute sur http://localhost:8080/")
 })
