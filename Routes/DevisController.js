93761c24340c765242556e60c0736e61179331bd
Styg <matthieu.hirth.68@gmail.com>
2021-05-12 15:25:47 +0200

    ajout page d'accueil

--- Routes/DevisController.js
+++ Routes/DevisController.js
@@ -19,6 +19,16 @@ const { FactureModel } = require('../Models/DevisModel');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+router.get('/', (req, res) => {
+    DevisModel.find((err, devis) => {
+        if (!err) {
+            // res.send(docs);
+            res.render("accueil", { devis: devis });
+            //console.log(devis[0].client);
+        }
+        else console.log("Error to get data : " + err);
+    })
+});
 
 router.get('/devis', (req, res) => {
     DevisModel.find((err, devis) => {
